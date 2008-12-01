3b3e26ef92a2a0978160c3efb9381b88f53223a1
Szymon Laszczyński <szymonlaszczynski@gmail.com>
2008-12-01 21:00:27 +0000

    Build reconstruction commit

---
+++ tavola-game/src/game/Snake.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package game;
+
+import interfaces.Direction;
+import interfaces.IPlayer;
+
+import java.util.LinkedList;
+
+/**
+ * @author sla
+ * 
+ */
+public final class Snake implements IPlayer {
+
+  private Direction direction;
+
+  protected Position head;
+
+  protected LinkedList<Position> body;
+
+  protected Position last;
+
+  public Snake(int width, int height) {
+    direction = Direction.RIGHT;
+    head = new Position(width / 2, height / 2);
+    body = new LinkedList<Position>();
+    last = new Position(width / 2 - 4, height / 2);
+    body.addFirst(new Position(width / 2 - 3, height / 2));
+    body.addFirst(new Position(width / 2 - 2, height / 2));
+    body.addFirst(new Position(width / 2 - 1, height / 2));
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see interfaces.Player#changeDirection(interfaces.Direction)
+   */
+  public void changeDirection(Direction direction) {
+    if (!this.direction.opposite(direction)) {
+      this.direction = direction;
+    }
+  }
+
+  /*
+   * Oblicz zmianę położenia głowy na podstawie aktualnego kierunku (direction).
+   */
+  private Position nextMove() {
+    switch (direction) {
+      case RIGHT:
+        return new Position(1, 0);
+      case LEFT:
+        return new Position(-1, 0);
+      case UP:
+        return new Position(0, 1);
+      case DOWN:
+        return new Position(0, -1);
+      default:
+        return new Position(0, 0);
+    }
+  }
+
+  public void move() {
+    body.addFirst(head);
+    last = body.getLast();
+    body.removeLast();
+    head.add(nextMove());
+  }
+}
