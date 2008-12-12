13582e9b2b75542807c1f406354a591b71f53848
Szymon Laszczyński <szymonlaszczynski@gmail.com>
2008-12-12 02:30:00 +0000

    tavola-game: - created new package "commons" - classes Direction & Position moved to package commons

---
+++ tavola-game/src/commons/Position.java
@@ -0,0 +1,30 @@
+/**
+ * Przechowuje dane na temat pozycji.
+ */
+package commons;
+
+/**
+ * @author sla
+ * 
+ */
+public final class Position {
+
+  public int horizontal;
+
+  public int vertical;
+
+  public Position(int horizontal, int vertical) {
+    this.vertical = vertical;
+    this.horizontal = horizontal;
+  }
+
+  public void add(Position p) {
+    vertical += p.vertical;
+    horizontal += p.horizontal;
+  }
+
+  @Override
+  public Position clone() {
+    return new Position(horizontal, vertical);
+  }
+}
